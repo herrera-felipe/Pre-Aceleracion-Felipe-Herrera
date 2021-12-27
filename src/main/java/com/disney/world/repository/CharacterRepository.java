3469c4bf16a73e5e460cdb16ebcde13393f4df6e
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-12-27 15:46:02 -0300

    Modified CharacterRepository

--- src/main/java/com/disney/world/repository/CharacterRepository.java
+++ src/main/java/com/disney/world/repository/CharacterRepository.java
@@ -1,11 +1,16 @@
 package com.disney.world.repository;
 
+import java.util.List;
+
 import com.disney.world.entity.CharacterEntity;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CharacterRepository extends JpaRepository<CharacterEntity, Long> {
     
+    // Metodo para la busqueda por filtros
+    public List<CharacterEntity> findAll(Specification<CharacterEntity> spec);
 }
