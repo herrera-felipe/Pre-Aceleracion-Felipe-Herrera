0b8b82cd049c80a7c232d7d8542f66799f6094a1
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-12-28 13:52:18 -0300

    Added findAll with Specifications

--- src/main/java/com/disney/world/repository/MovieRepository.java
+++ src/main/java/com/disney/world/repository/MovieRepository.java
@@ -1,11 +1,16 @@
 package com.disney.world.repository;
 
+import java.util.List;
+
 import com.disney.world.entity.MovieEntity;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieRepository extends JpaRepository<MovieEntity, Long> {
     
+    // Metodo para la busqueda por filtros
+    public List<MovieEntity> findAll(Specification<MovieEntity> spec);
 }
