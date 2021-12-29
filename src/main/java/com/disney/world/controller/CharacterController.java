976da5fcb9b4c9a583ee04359aecec684dfe0a4e
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-12-29 17:24:56 -0300

    Added update PutMapping

--- src/main/java/com/disney/world/controller/CharacterController.java
+++ src/main/java/com/disney/world/controller/CharacterController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,20 +26,20 @@ public class CharacterController {
     @Autowired
     private CharacterService characterService;
 
-    @GetMapping
+    @GetMapping("/details")
     public ResponseEntity<List<CharacterDTO>> getAll() {
         List<CharacterDTO> characterList = this.characterService.getAllCharacters();
         return ResponseEntity.ok().body(characterList);
     }
 
-    @GetMapping("basic")
+    @GetMapping
     public ResponseEntity<List<CharacterBasicDTO>> getAllBasicData() {
         List<CharacterBasicDTO> characterBasicList = this.characterService.getAllCharactersBasicData();
         return ResponseEntity.ok().body(characterBasicList);
     }
 
      // Busqueda por filtros
-     @GetMapping("filters")
+     @GetMapping("/filters")
      public ResponseEntity<List<CharacterDTO>> getDetailsByFilters(
              @RequestParam(required = false) String name,
              @RequestParam(required = false) Integer age,
@@ -53,6 +54,12 @@ public class CharacterController {
     public ResponseEntity<CharacterDTO> save(@RequestBody CharacterDTO dto) {
         CharacterDTO characterSaved = this.characterService.save(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(characterSaved);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CharacterDTO> update(@PathVariable Long id, @RequestBody CharacterDTO dto) {
+        CharacterDTO resultDTO = this.characterService.update(id, dto);
+        return ResponseEntity.ok().body(resultDTO);
     }
 
     // EndPoint para el Soft Delete mediante id
