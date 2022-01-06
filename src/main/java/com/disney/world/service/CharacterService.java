f0b7abe1976a860494750f01b89a4bf465208ba3
Felipe Herrera <herrerafelipe.093@gmail.com>
2022-01-06 13:58:04 -0300

    Added addMovie & removeMovie methods

--- src/main/java/com/disney/world/service/CharacterService.java
+++ src/main/java/com/disney/world/service/CharacterService.java
@@ -8,19 +8,12 @@ import com.disney.world.dto.CharacterDTO;
 public interface CharacterService {
 
     public CharacterDTO save(CharacterDTO dto);
-
     public CharacterDTO update(Long id, CharacterDTO dto);
-
     public void delete(Long id);
-
     public List<CharacterDTO> getAllCharacters();
-
     public List<CharacterBasicDTO> getAllCharactersBasicData();
-
     public List<CharacterDTO> getByFilters(String name, Integer age, List<Long> movies, String order);
-
     public void addMovie(Long id, Long idMovie);
-
     public void removeMovie(Long id, Long idMovie);
 
 }
